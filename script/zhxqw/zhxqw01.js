2f3e2627577041d8f2cf962edacf3e3d13045822
xzxiaoshan <365384722@qq.com>
2023-09-22 10:08:57 +0800

    Update zhxqw01.js

--- script/zhxqw/zhxqw01.js
+++ script/zhxqw/zhxqw01.js
@@ -1,8 +1,8 @@
 console.log('>>>>>>>>>>>> 最红星期五商品详情页按钮提前显示 script start');
 var resBody = JSON.parse($response.body);
 
-//05是未开始，12是次数用完
-if(resBody.data.buttonType == '05'){
+//05是???，12是次数用完，02是未开始
+if(resBody.data.buttonType == '02'){
   resBody.data.buttonType = '01';
 }
 
